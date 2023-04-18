5dca0ac682750c9b08f562952b6b057a59b5dc2c
MmaGod1 <gmmaduchukwu@gmail.com>
2023-04-18 21:25:53 +0000

    adding http custom header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#Just as in task #0, we’d like you to automate the task of creating a custom HTTP header response, but with Puppet.
+file { '/etc/nginx/sites-enabled/default':
+	ensure => present,
+  }->
+file-line { 'Apend a line to /etc/nginx/sites-enabled/default:
+	path => '/etc/nginx/sites-enabled/default',
+	line => 'add_header X-Served-By \$hostname',
+	after => '^I wrote the codes below'.
+}
