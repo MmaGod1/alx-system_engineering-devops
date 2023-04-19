762a7ee9262f3ad4f4fad2d187121ea969162d73
MmaGod1 <gmmaduchukwu@gmail.com>
2023-04-19 17:57:38 +0000

    automate with puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,22 @@
-#Just as in task #0, we’d like you to automate the task of creating a custom HTTP header response, but with Puppet.
-file { '/etc/nginx/sites-enabled/default':
-	ensure => present,
-  }->
-file-line { 'Apend a line to /etc/nginx/sites-enabled/default:
-	path => '/etc/nginx/sites-enabled/default',
-	line => 'add_header X-Served-By \$hostname',
-	after => '^I wrote the codes below'.
+#Just as qin task #0, create a custom HTTP header response, with Puppet.
+exec {'apt-get':
+ 	provider => shell,
+	command  => 'sudo apt-get -y update',
+}
+
+package {'nginx':
+        ensure => installed,
+        require => Exec['apt-update'],                         }
+
+file { '/etc/nginx/sites-enabled/default':                         ensure => present,                                         }->
+file-line { 'Apend a line to /etc/nginx/sites-enabled/defau
+lt:
+        path => '/etc/nginx/sites-enabled/default',
+        line => 'add_header X-Served-By $hostname',                after => '^I wrote the codes below'.
+        }->
+exec {'run':                                                       command => '/usr/sbin/service nginx restart',
+}
+exec { 'restart Nginx':
+	provider => shell,
+	command  => 'sudo service nginx restart',
 }
