78bedaf50b69ea262ce95ba737bc02e02929a713
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-07-07 15:07:28 +0100

    Update resources name

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # Configure SSH client file
 
-file_line { 'Turn off passwd auth':
+file_line { 'Disable password':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
   replace => true,
 }
 
-file_line { 'Delare identity file':
+file_line { 'Use private key':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '     IdentityFile ~/.ssh/school',
