dd0c6116afc444350f34f69691df14e1ed9e554c
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-08-29 12:38:29 +0100

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,18 +13,25 @@ if __name__ == "__main__":
         print(f"Usage: {sys.argv[0]} <employee_id>")
         sys.exit(1)
 
-    employee_id = sys.argv[1]
+    employee_id = int(sys.argv[1])
     user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    todos_url = (
+            f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    )
+    employee_data = requests.get(user_url).json()
+    todos_data = requests.get(todos_url).json()
 
-    # Fetch employee data
-    employee_response = requests.get(user_url)
-    employee_data = employee_response.json()
-    employee_name = employee_data.get("name", "")
-
-    # Fetch todos
-    todos_response = requests.get(todos_url)
-    todos_data = todos_response.json()
+    employee_name = employee_data["name"]
 
     # Write data to CSV
-    with open(f"{employee
+    with open(f"{employee_id}.csv", mode='w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in todos_data:
+            writer.writerow([
+                employee_id,
+                employee_name,
+                str(task["completed"]),
+                task["title"]
+            ])
+
+    print(f"Data exported to {employee_id}.csv")
