0bb5ace674cee1dff9b854f897564dee089fecbc
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-04 19:47:58 +0100

    Create 0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,19 @@
+# 0-strace_is_your_friend.pp
+file { '/path/to/missing/file':
+  ensure  => 'file',
+  source  => 'puppet:///modules/your_module/missing_file',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
+}
+
+service { 'apache2':
+  ensure => 'running',
+  enable => true,
+  notify => Exec['restart_apache'],
+}
+
+exec { 'restart_apache':
+  command => '/bin/systemctl restart apache2',
+  refreshonly => true,
+}
