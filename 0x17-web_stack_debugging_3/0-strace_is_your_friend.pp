06363d85f13546d289199d32390023f238f0dcfa
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-09-04 21:04:11 +0100

    Fix Apache error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,25 +1,6 @@
-# 0-strace_is_your_friend.pp
+# Why Apache is returning a 500 error.
 
-file { '/etc/apache2/sites-available/your_site.conf':
-  ensure  => file,
-  content => <<-EOT
-    # Apache virtual host configuration
-    <VirtualHost *:80>
-      DocumentRoot /var/www/html
-      # Remove or update any lines referencing missing files or incorrect configurations
-    </VirtualHost>
-  EOT
-  require => Package['apache2'],
-}
-
-# Restart Apache after configuration changes
-service { 'apache2':
-  ensure => running,
-  enable => true,
-  notify => Exec['restart_apache'],
-}
-
-exec { 'restart_apache':
-  command     => '/usr/bin/systemctl restart apache2', # Correct path to systemctl
-  refreshonly => true,
+exec { 'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
